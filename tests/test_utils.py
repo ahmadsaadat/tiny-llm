628031f844db94eb0c7f5bfc49e3d95f181a3bb1
Ahmad Saadat <47593578+ahmadsaadat@users.noreply.github.com>
2026-06-02 18:14:16 -0500

    add another test

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,10 +1,19 @@
 import unittest
 
-from tiny_llm.utils import read_text
+from tiny_llm.utils import prepare_text, read_text
 
 
 class TestReadText(unittest.TestCase):
+    FILENAME = "tiny_llm/input_data/mock_text.txt"
+    EXPECTED = """hello 1\nhello 2\nhello 3"""
+
     def test_read_text(self):
-        filename = "tiny_llm/input_data/mock_text.txt"
-        raw_text = read_text(filename=filename)
-        print(raw_text)
+        assert read_text(filename=self.FILENAME) == self.EXPECTED
+
+
+class TestPrepareText(unittest.TestCase):
+    RAW_TEXT = """hello 1\nhello 2\nhello 3"""
+    EXPECTED = ["hello 1 <END>", "hello 2 <END>", "hello 3 <END>"]
+
+    def test_prepare_text(self):
+        assert prepare_text(raw_text=self.RAW_TEXT) == self.EXPECTED
