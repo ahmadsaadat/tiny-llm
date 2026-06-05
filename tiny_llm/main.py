694d8c6580ab21d4f49ff8297390bcf1f9c90c6e
Ahmad Saadat <47593578+ahmadsaadat@users.noreply.github.com>
2026-06-04 23:26:07 -0500

    checkpoint

--- tiny_llm/main.py
+++ tiny_llm/main.py
@@ -1,6 +1,9 @@
+import torch
+
 from tiny_llm.modules.a_data import Data
 from tiny_llm.modules.b_tables import Table
 from tiny_llm.modules.c_trainer import Trainer
+from tiny_llm.modules.d_transformer_block import TransformerBlock
 
 
 def train():
@@ -39,7 +42,53 @@ def train():
 
 
 def infer(user_input: str):
-    pass
+    data = Data(
+        filename_input_data="tiny_llm/io/tiny_stories.txt",
+        filename_output_data="tiny_llm/io/tiny_stories_tokenizer.json",
+        sequence_length=16,
+        sequence_size_max=20_000,
+    )
+
+    checkpoint = torch.load(
+        "tiny_llm/io/tiny_stories_gpt.pt",
+        map_location="mps",
+        weights_only=False,
+    )
+
+    tables = checkpoint["tables"]
+
+    token_ids = data.tokenizer.encode(user_input.lower()).ids
+
+    for _ in range(10):
+        input_ids = token_ids[-16:]
+
+        while len(input_ids) < 16:
+            input_ids.insert(0, 0)
+
+        inputs = torch.tensor([input_ids], device="mps")
+
+        # embeddings + positions
+        X = tables[0].table_embedding[inputs]
+        Z = X + tables[0].table_position
+
+        # transformer blocks
+        for table in tables:
+            Z = TransformerBlock(
+                table,
+                64,
+                torch.device("mps"),
+            ).forward(Z)
+
+        # logits
+        last_token_vector = Z[:, -1]
+        logits = last_token_vector @ tables[0].table_lm_head
+
+        # greedy pick
+        next_token_id = torch.argmax(logits, dim=-1).item()
+
+        token_ids.append(next_token_id)
+
+    return data.tokenizer.decode(token_ids)
 
 
 if __name__ == "__main__":
